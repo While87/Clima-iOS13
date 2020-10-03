5d4b1f3f78baa636842ff1c66dec02c00e0edb6a
VladimirG <proton87@yandex.ru>
2020-10-03 22:52:42 +0300

    Add files via upload

---
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Vladimir Gorbunov on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController {
+    
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.delegate = self
+        //Запрос requestLocation выполняется только после определения делегата и его методов(DidUpdate and DidFail)
+        locationManager.requestLocation()
+        
+        
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+        
+    }
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
+    
+
+
+//MARK: - UITextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate {
+    //Проверка введенныч данных в поле поиска
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != ""
+        {
+            searchTextField.placeholder = "Search"
+            return true
+        } else {
+            searchTextField.placeholder = "Please type something"
+            return false
+        }
+    }
+    
+    //Действие после завершения ввода в поиске и нажатии на !клавиатуре! команды на выполнение
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    //Очистка поля поиска после нажатия кнопки деиствия(завершения ввода) и скрытие клавиатуры
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        //Очищаем содержимое
+        searchTextField.text = ""
+    }
+}
+//MARK: - WeatherManagerDelegate
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ _weatherManager: WeatherManager, weather: WeatherModel ){
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    func didFailWhithError(_ error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - CoreLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print(error)
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let long = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: long)
+        }
+    }
+}
---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Vladimir Gorbunov on 9/30/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Weatherdata: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
+
